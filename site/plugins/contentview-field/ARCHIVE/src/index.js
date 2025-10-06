3f9d258774a718102beabeb3b0343f4daef6c713
2inchesofwater <no@thanks.com>
2025-10-07 09:07:58 +1000

    Initial push of local folder contents to scraper branch

--- site/plugins/contentview-field/ARCHIVE/src/index.js
+++
@@ -1,8 +0,0 @@
-import ContentView from "./components/ContentView.vue";
-
-// Register the field component
-panel.plugin("dangilmore/contentview-field", {
-  fields: {
-    contentview: ContentView
-  }
-});
